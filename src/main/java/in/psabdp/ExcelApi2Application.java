046a75e3d009c965b8620140fadc51c1104abfd8
Linton <lintonchoudhury90@gmail.com>
2025-03-02 09:57:27 +0530

    intial commit

---
+++ src/main/java/in/psabdp/ExcelApi2Application.java
@@ -0,0 +1,13 @@
+package in.psabdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelApi2Application.class, args);
+	}
+
+}
